771d4841b6b3765479c11a1decf216d2fabaa88f
João Heitor <jhmartinsrdo@gmail.com>
2023-04-07 13:37:19 -0300

    feat: jobdetails functionality

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,14 +1,14 @@
-import React from "react";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
 import {
-  Text,
   View,
+  Text,
   SafeAreaView,
   ScrollView,
   ActivityIndicator,
   RefreshControl,
 } from "react-native";
-import { Stack, useRouter, useSearchParams } from "expo-router";
-import { useCallback, useState } from "react";
+
 import {
   Company,
   JobAbout,
@@ -17,22 +17,55 @@ import {
   ScreenHeaderBtn,
   Specifics,
 } from "../../components";
-import { COLORS, icons, SIZES } from "../../components";
+import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
-const tabs = ["Sobre", "Qualificações", "Responsabilidades"];
+const tabs = ["About", "Qualifications", "Responsibilities"];
 
 const JobDetails = () => {
   const params = useSearchParams();
   const router = useRouter();
+
   const { data, isLoading, error, refetch } = useFetch("job-details", {
     job_id: params.id,
   });
 
-  const [refreshing, setRefreshing] = useState(false);
   const [activeTab, setActiveTab] = useState(tabs[0]);
+  const [refreshing, setRefreshing] = useState(false);
 
-  const onRefresh = () => {};
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    refetch();
+    setRefreshing(false);
+  }, []);
+
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return (
+          <Specifics
+            title="Qualifications"
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+
+      case "About":
+        return (
+          <JobAbout info={data[0].job_description ?? "No data provided"} />
+        );
+
+      case "Responsibilities":
+        return (
+          <Specifics
+            title="Responsibilities"
+            points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+          />
+        );
+
+      default:
+        return null;
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -54,6 +87,7 @@ const JobDetails = () => {
           headerTitle: "",
         }}
       />
+
       <>
         <ScrollView
           showsVerticalScrollIndicator={false}
@@ -64,14 +98,14 @@ const JobDetails = () => {
           {isLoading ? (
             <ActivityIndicator size="large" color={COLORS.primary} />
           ) : error ? (
-            <Text>Algo deu errado</Text>
+            <Text>Something went wrong</Text>
           ) : data.length === 0 ? (
-            <Text>Sem data</Text>
+            <Text>No data available</Text>
           ) : (
             <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
               <Company
                 companyLogo={data[0].employer_logo}
-                jobTtitle={data[0].job_title}
+                jobTitle={data[0].job_title}
                 companyName={data[0].employer_name}
                 location={data[0].job_country}
               />
@@ -81,12 +115,21 @@ const JobDetails = () => {
                 activeTab={activeTab}
                 setActiveTab={setActiveTab}
               />
+
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
+
+        <JobFooter
+          url={
+            data[0]?.job_google_link ??
+            "https://careers.google.com/jobs/results/"
+          }
+        />
       </>
     </SafeAreaView>
   );
 };
 
-export default [JobDetails];
+export default JobDetails;
