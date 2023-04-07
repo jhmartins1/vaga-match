1e8d1bf8d87fdf184c1b7c6a22a045df174f5362
João Heitor <jhmartinsrdo@gmail.com>
2023-04-07 13:37:47 -0300

    feat: jobdetails components

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -13,14 +13,16 @@ const Company = ({ companyLogo, jobTitle, companyName, location }) => {
           source={{
             uri: checkImageURL(companyLogo)
               ? companyLogo
-              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqz05H.jpg",
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
           style={styles.logoImage}
         />
       </View>
+
       <View style={styles.jobTitleBox}>
         <Text style={styles.jobTitle}>{jobTitle}</Text>
       </View>
+
       <View style={styles.companyInfoBox}>
         <Text style={styles.companyName}>{companyName} / </Text>
         <View style={styles.locationBox}>
